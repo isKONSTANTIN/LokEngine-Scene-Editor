8998d2bfcf402456601f3c67a2327f1b8c49220b
lokin135 <lokin135help@gmail.com>
2019-09-16 16:38:44 +0300

    Camera Movement: add scroll

--- src/LokEngineSceneEditor/SceneInteraction/CameraMovement.java
+++ src/LokEngineSceneEditor/SceneInteraction/CameraMovement.java
@@ -3,19 +3,31 @@ package LokEngineSceneEditor.SceneInteraction;
 import LokEngine.Render.Camera;
 import LokEngine.Tools.RuntimeFields;
 import LokEngine.Tools.Utilities.Vector2i;
+import org.lwjgl.input.Mouse;
 
 public class CameraMovement {
 
     static Vector2i lastPos = new Vector2i();
     static boolean lastStatus;
+
+    static float view = 1;
+
     public static void update(Camera camera){
 
         if (RuntimeFields.getMouseStatus().getPressedStatus() && lastStatus){
-            camera.position.x -= (RuntimeFields.getMouseStatus().getMousePosition().x - lastPos.x) * 0.001f;
-            camera.position.y -= (RuntimeFields.getMouseStatus().getMousePosition().y - lastPos.y) * 0.001f;
+            camera.position.x -= (RuntimeFields.getMouseStatus().getMousePosition().x - lastPos.x) * 0.001f * view;
+            camera.position.y -= (RuntimeFields.getMouseStatus().getMousePosition().y - lastPos.y) * 0.001f * view;
             lastPos = RuntimeFields.getMouseStatus().getMousePosition();
         }else if (!lastStatus){
             lastPos = RuntimeFields.getMouseStatus().getMousePosition();
+        }
+
+        int wheel = Mouse.getDWheel();
+
+        if (wheel != 0){
+            view += wheel / 1000f * view;
+            view = Math.max(view, 0.001f);
+            camera.setFieldOfView(view);
         }
 
         lastStatus = RuntimeFields.getMouseStatus().getPressedStatus();
