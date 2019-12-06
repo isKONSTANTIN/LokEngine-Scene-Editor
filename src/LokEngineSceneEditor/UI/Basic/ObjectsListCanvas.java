593bfd450600dfd475302450fda3f62e772c67c2
lokin135 <lokin135help@gmail.com>
2019-12-06 22:12:26 +0300

    ObjectListCanvas: add scroll limit

--- src/LokEngineSceneEditor/UI/Basic/ObjectsListCanvas.java
+++ src/LokEngineSceneEditor/UI/Basic/ObjectsListCanvas.java
@@ -33,7 +33,7 @@ public class ObjectsListCanvas extends GUICanvas {
         panel = new GUIPanel(new Vector2i(),new Vector2i(), new Color(0.25f,0.25f, 0.25f,0.6f));
         panel.setSize(guiObject -> this.getSize());
 
-        scrollCanvas = new GUIScrollCanvas(new Vector2i(0,10),new Vector2i());
+        scrollCanvas = new GUIScrollCanvas(new Vector2i(0,10),new Vector2i(),null,new Vector2i());
         scrollCanvas.setSize(guiObject -> new Vector2i(this.getSize().x,this.getSize().y - guiObject.getPosition().y));
 
         textDrawer = new GUIFreeTextDrawer();
@@ -56,7 +56,7 @@ public class ObjectsListCanvas extends GUICanvas {
         for (int i = 0; i < objectsCount; i++){
             SceneObject sceneObject = scene.getObjectByID(i);
             Vector2i textPos = new Vector2i(0,textGap.y * (i+1));
-            boolean selected = parentProperties.window.getMouse().inField(new Vector2i(properties.globalPosition.x + textPos.x, properties.globalPosition.y + textPos.y + textGap.y), new Vector2i(getSize().x,textGap.y));
+            boolean selected = parentProperties.window.getMouse().inField(new Vector2i(scrollCanvas.properties.globalPosition.x + textPos.x, scrollCanvas.properties.globalPosition.y + textPos.y), new Vector2i(getSize().x,textGap.y));
 
             if (selected && parentProperties.window.getMouse().getPressedStatus()){
                 HighlightedObject.highlight(sceneObject,i);
