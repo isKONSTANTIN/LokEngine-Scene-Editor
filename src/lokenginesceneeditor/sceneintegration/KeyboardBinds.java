7dfb772366bc9fb03f119985000de5af98422f59
lokin135 <lokin135help@gmail.com>
2019-12-22 19:05:18 +0300

    KeyboardBinds: more actions

--- src/lokenginesceneeditor/sceneintegration/KeyboardBinds.java
+++ src/lokenginesceneeditor/sceneintegration/KeyboardBinds.java
@@ -1,18 +1,66 @@
 package lokenginesceneeditor.sceneintegration;
 
 import lokenginesceneeditor.LESEApplication;
+import org.lwjgl.util.vector.Vector2f;
 import ru.lokincompany.lokengine.applications.applications.Application;
 import ru.lokincompany.lokengine.applications.applications.ApplicationDefault;
 import ru.lokincompany.lokengine.render.Camera;
 import ru.lokincompany.lokengine.sceneenvironment.SceneObject;
 import ru.lokincompany.lokengine.tools.input.Keyboard;
 import ru.lokincompany.lokengine.tools.input.Mouse;
+import ru.lokincompany.lokengine.tools.utilities.Vector2i;
 
 import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyboardBinds {
 
-    static boolean lastPressedCKey;
+    static boolean lastCKeyPressed;
+    static boolean lastDKeyPressed;
+    static Vector2f mouseDistance;
+
+    static boolean checkObjectActions(Keyboard keyboard, Camera camera, Mouse mouse) {
+        SceneObject sceneObject = HighlightedObject.getHighlightedObject();
+        if (sceneObject == null || !keyboard.isKeyDown(GLFW_KEY_LEFT_CONTROL)) return false;
+
+        boolean actionHappened = false;
+        boolean CKeyPressed = keyboard.isKeyDown(GLFW_KEY_C);
+        boolean DKeyPressed = keyboard.isKeyDown(GLFW_KEY_D);
+        boolean ZKeyPressed = keyboard.isKeyDown(GLFW_KEY_Z);
+
+        boolean mousePressed = mouse.getPressedStatus();
+
+        if (CKeyPressed && !lastCKeyPressed) {
+            HighlightedObject.copyObject();
+            actionHappened = true;
+        } else if (DKeyPressed && !lastDKeyPressed) {
+            HighlightedObject.deleteObject();
+            actionHappened = true;
+        } else if (ZKeyPressed) {
+            Vector2i mouseScreenPos = mouse.getMousePosition();
+            Vector2f mouseScenePos = camera.screenPointToScene(mouseScreenPos);
+
+            if (mouseDistance == null){
+                if (mousePressed){
+                    mouseDistance = new Vector2f(0,0);
+                }else
+                    mouseDistance = new Vector2f(sceneObject.position.x - mouseScenePos.x,sceneObject.position.y - mouseScenePos.y);
+            }else if (mousePressed){
+                mouseDistance.set(0, 0);
+            }
+
+            sceneObject.position = new Vector2f(mouseScenePos.x + mouseDistance.x, mouseScenePos.y + mouseDistance.y);
+            actionHappened = true;
+        }
+
+        if (!ZKeyPressed){
+            mouseDistance = null;
+        }
+
+        lastCKeyPressed = CKeyPressed;
+        lastDKeyPressed = DKeyPressed;
+
+        return actionHappened;
+    }
 
     public static void update() {
         LESEApplication application = LESEApplication.getInstance();
@@ -20,7 +68,9 @@ public class KeyboardBinds {
         Mouse mouse = application.window.getMouse();
         Camera camera = application.window.getCamera();
 
-        if (mouse.inField(application.sceneEditor.objectsList.properties.globalPosition, application.sceneEditor.objectsList.properties.size) || mouse.inField(application.sceneEditor.objectProperties.properties.globalPosition, application.sceneEditor.objectProperties.properties.size))
+        if (    checkObjectActions(keyboard, camera, mouse) ||
+                mouse.inField(application.sceneEditor.objectsList.properties.globalPosition, application.sceneEditor.objectsList.properties.size) ||
+                mouse.inField(application.sceneEditor.objectProperties.properties.globalPosition, application.sceneEditor.objectProperties.properties.size))
             return;
 
         float extraSpeed = keyboard.isKeyDown(GLFW_KEY_LEFT_SHIFT) ? 3 : 1;
@@ -33,17 +83,5 @@ public class KeyboardBinds {
         camera.position.x -= keyboard.isKeyDown(GLFW_KEY_A) ? camera.fieldOfView * application.applicationRuntime.getDeltaTime() * extraSpeed / 90f : 0;
         camera.position.y += keyboard.isKeyDown(GLFW_KEY_W) ? camera.fieldOfView * application.applicationRuntime.getDeltaTime() * extraSpeed / 90f : 0;
         camera.position.y -= keyboard.isKeyDown(GLFW_KEY_S) ? camera.fieldOfView * application.applicationRuntime.getDeltaTime() * extraSpeed / 90f : 0;
-
-        SceneObject sceneObject = HighlightedObject.getHighlightedObject();
-
-        if (sceneObject != null) {
-            if (keyboard.isKeyDown(GLFW_KEY_LEFT_CONTROL))
-                if (keyboard.isKeyDown(GLFW_KEY_C) && !lastPressedCKey) {
-                    HighlightedObject.copyObject();
-                } else if (keyboard.isKeyDown(GLFW_KEY_Z))
-                    sceneObject.position = camera.screenPointToScene(mouse.getMousePosition());
-        }
-
-        lastPressedCKey = keyboard.isKeyDown(GLFW_KEY_C);
     }
 }
